52d4bbf7d97b1efd2175b02ab1029bd3ed2aa3bb
Gideon <gideonkarasek@gmail.com>
2019-11-12 14:13:51 -0600

    Push eda changes to repo

--- data/data_cleaner.py
+++ data/data_cleaner.py
@@ -10,12 +10,25 @@ def removing_na_values(df, column_name):
     return df
 
 def money_values_to_integer(df, column_name):
-    series = df[column_name]
-    series_string = series.to_string(index=False,header=False)
+    series_string = df[column_name].to_string(index=False,header=False)
     string_to_convert = series_string.replace('$', '')
     string_to_convert = string_to_convert.replace(',', '')
     list_to_convert = string_to_convert.split()
     cleaned_series = pd.Series(list_to_convert)
     money_as_int = pd.to_numeric(cleaned_series)
     df[column_name] = money_as_int
+    return df
+
+def clean_director_columns(df):
+    df.reset_index(inplace=True)
+    df.worldwide_gross.astype('int64')
+    df.rename(columns={'primary_name': 'Director','worldwide_gross': 'Average Worldwide Box Office',
+                       'profitability': 'Average Profit per Movie', 'num_of_movies': 'Number of Movies'}, inplace=True)
+    return df
+
+def clean_actor_columns(df):
+    df.reset_index(inplace=True)
+    df.worldwide_gross.astype('int64')
+    df.rename(columns={'primary_name': 'Actor','worldwide_gross': 'Average Worldwide Box Office',
+                       'profitability': 'Average Profit per Movie', 'num_of_movies': 'Number of Movies'}, inplace=True)
     return df
